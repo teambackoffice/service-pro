c0f232c2ef40dcf22dc3d4ec0720c863f49db40f
jiloysss <jangeles@bai.ph>
2020-08-21 17:27:45 +0800

    Update

--- service_pro/service_pro/report/daily_cash_book/daily_cash_book.py
+++ service_pro/service_pro/report/daily_cash_book/daily_cash_book.py
@@ -24,7 +24,7 @@ def execute(filters=None):
 	if filters.get("pos_profile"):
 		condition += " and "
 
-		condition += "SI.pos_profile='{0}'".format(filters.get("pos_profile"))
+		condition += "SI.pos_profile='{0}' and SI.is_pos = 1".format(filters.get("pos_profile"))
 
 	if filters.get("warehouse"):
 		condition += " and "
@@ -46,13 +46,15 @@ def execute(filters=None):
 	query = """ SELECT 
  					SI.posting_date as date,
  					SI.name as si_no,
- 					(SELECT allocated_amount FROM `tabSales Invoice Advance` AS A WHERE A.parent = SI.name LIMIT 1) as advance,
  					(SELECT customer_name FROM `tabCustomer` AS C WHERE C.name = SI.customer) as customer_name,
- 					SI.grand_total as grand_total,
  					(SELECT sales_person FROM `tabSales Team` AS ST WHERE ST.parent = SI.name LIMIT 1) as sales_man_agent,
+					(SELECT mode_of_payment FROM `tabSales Invoice Payment` AS SIP WHERE SIP.parent = SI.name LIMIT 1) as mop,
+					SI.total_taxes_and_charges as vat,
+ 					(SELECT allocated_amount FROM `tabSales Invoice Advance` AS A WHERE A.parent = SI.name LIMIT 1) as advance,
+ 					SI.total as net_total,
+ 					SI.grand_total as grand_total,
  					(SELECT incentives FROM `tabSales Team` AS ST WHERE ST.parent = SI.name LIMIT 1) as insentive,
- 					(SELECT mode_of_payment FROM `tabSales Invoice Payment` AS SIP WHERE SIP.parent = SI.name LIMIT 1) as mop,
- 					SI.grand_total - (SELECT mode_of_payment FROM `tabSales Invoice Payment` AS SIP WHERE SIP.parent = SI.name LIMIT 1) as net_amount,
+ 					SI.total - (SELECT incentives FROM `tabSales Team` AS ST WHERE ST.parent = SI.name LIMIT 1) as net_amount,
  					SI.status as status
 				FROM `tabSales Invoice` AS SI {0} WHERE SI.docstatus = 1 {1}""".format(query_,condition)
 	print(query)
@@ -66,12 +68,15 @@ def get_columns():
 	columns = [
 		{"label": "Date", "fieldname": "date", "fieldtype": "Date","width": "100"},
 		{"label": "SI No", "fieldname": "si_no", "fieldtype": "Link","options": "Sales Invoice","width": "150"},
+		{"label": "Customer Name", "fieldname": "customer_name", "fieldtype": "Data", "width": "150"},
+		{"label": "Sales Man/Agent", "fieldname": "sales_man_agent", "fieldtype": "Data", "width": "150"},
+		{"label": "MOP", "fieldname": "mop", "fieldtype": "Data", "width": "100"},
+		{"label": "VAT", "fieldname": "vat", "fieldtype": "Float","precision": "2", "width": "100"},
 		{"label": "Advance", "fieldname": "advance", "fieldtype": "Float","precision": "2","width": "100"},
-		{"label": "Customer Name", "fieldname": "customer_name", "fieldtype": "Data","width": "150"},
+		{"label": "Net Total", "fieldname": "net_total", "fieldtype": "Float","precision": "2","width": "100"},
+
 		{"label": "Grand Total", "fieldname": "grand_total","fieldtype": "Float","precision": "2","width": "100"},
-		{"label": "Sales Man/Agent", "fieldname": "sales_man_agent", "fieldtype": "Data","width": "150"},
 		{"label": "Insentive", "fieldname": "insentive", "fieldtype": "Float","precision": "2","width": "100"},
-		{"label": "MOP", "fieldname": "mop", "fieldtype": "Data","width": "100"},
 		{"label": "Net Amount", "fieldname": "net_amount", "fieldtype": "Float","precision": "2","width": "100"},
 		{"label": "Status", "fieldname": "status", "fieldtype": "Data","width": "100"},
 	]

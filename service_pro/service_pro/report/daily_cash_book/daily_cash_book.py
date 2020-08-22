0ae8a2d8df6ab1010f81a1125c14e6f0682a9631
jiloysss <jangeles@bai.ph>
2020-08-22 14:35:59 +0800

    Update

--- service_pro/service_pro/report/daily_cash_book/daily_cash_book.py
+++ service_pro/service_pro/report/daily_cash_book/daily_cash_book.py
@@ -46,6 +46,7 @@ def execute(filters=None):
 	query = """ SELECT 
  					SI.posting_date as date,
  					SI.name as si_no,
+ 					SI.customer,
  					(SELECT customer_name FROM `tabCustomer` AS C WHERE C.name = SI.customer) as customer_name,
  					(SELECT sales_person FROM `tabSales Team` AS ST WHERE ST.parent = SI.name LIMIT 1) as sales_man_agent,
 					(SELECT mode_of_payment FROM `tabSales Invoice Payment` AS SIP WHERE SIP.parent = SI.name LIMIT 1) as mop,
@@ -56,37 +57,50 @@ def execute(filters=None):
  					SI.status as status
 				FROM `tabSales Invoice` AS SI {0} WHERE SI.docstatus = 1 {1}""".format(query_,condition)
 
-	data = frappe.db.sql(query,as_dict=1)
-	for i in data:
+	datas = frappe.db.sql(query,as_dict=1)
+	new_data = []
+	for i in datas:
+		i['advance'] = 0
+		i['net_amount'] = i.net_total - i.insentive if i.insentive else i.net_total
+		new_data.append(i)
 
-		if filters.get("from_date") and filters.get("to_date") and filters.get("pos_profile"):
-			jv = frappe.db.sql(""" 
+		# i['advance'] = 0
+		if filters.get("pos_profile"):
+			jv_query = """ 
 							SELECT * FROM `tabJournal Entry`AS JE 
-							INNER JOIN `tabJournal Entry Item` AS JEI ON JEI.parent = JE.name 
-							WHERE JE.posting_date BETWEEN %s and %s 
+							INNER JOIN `tabJournal Entry Account` AS JEI ON JEI.parent = JE.name 
+							WHERE JE.posting_date = '{0}' 
 								and JEI.is_advance = 'Yes' 
 								and JEI.party_type = 'Customer' 
-								and JEI.party = %s and JE.docstatus=1""", (filters.get("from_date"),filters.get("to_date"),i.customer), as_dict=1)
+								and JEI.party = '{1}' and JE.docstatus=1""".format(i.date,i.customer)
+			print(jv_query)
+			jv = frappe.db.sql(jv_query, as_dict=1)
+			print(jv)
 			if len(jv) > 0:
-				i['advance'] = jv[0].credit_in_account_currency
-
-		if ('advance' in i and i.advance == 0 and i.insentive == 0) or (i.advance == 0 and i.insentive > 0):
-			i['net_amount'] = i.net_total
-		elif 'advance' in i and  i.advance > 0 and i.insentive == 0:
-			i['net_amount'] = i.net_total + i.advance
-		elif 'advance' in i and  i.advance == 0:
-			i['net_amount'] = i.net_total - i.insentive
-	return columns, data
+				new_data.append({
+					"si_no": jv[0].parent,
+					"advance":jv[0].credit_in_account_currency,
+					"net_amount":jv[0].credit_in_account_currency,
+				})
+		# if 'advance' in i and i.insentive:
+		# 	if (i.advance == 0 and i.insentive == 0) or (i.advance == 0 and i.insentive > 0):
+		# 		i['net_amount'] = i.net_total
+		# 	elif i.advance > 0 and i.insentive == 0:
+		# 		i['net_amount'] = i.net_total + i.advance
+		# 	elif i.advance == 0:
+		# 		i['net_amount'] = i.net_total - i.insentive
+	return columns, new_data
 
 
 def get_columns():
 	columns = [
 		{"label": "Date", "fieldname": "date", "fieldtype": "Date","width": "100"},
-		{"label": "Advance", "fieldname": "advance", "fieldtype": "Float", "precision": "2", "width": "100"},
 		{"label": "SI No", "fieldname": "si_no", "fieldtype": "Link","options": "Sales Invoice","width": "150"},
 		{"label": "Customer Name", "fieldname": "customer_name", "fieldtype": "Data", "width": "150"},
 		{"label": "Sales Man/Agent", "fieldname": "sales_man_agent", "fieldtype": "Data", "width": "150"},
 		{"label": "MOP", "fieldname": "mop", "fieldtype": "Data", "width": "120"},
+		{"label": "Advance", "fieldname": "advance", "fieldtype": "Float", "precision": "2", "width": "100"},
+
 		{"label": "Net Total", "fieldname": "net_total", "fieldtype": "Float","precision": "2","width": "100"},
 		{"label": "VAT", "fieldname": "vat", "fieldtype": "Float", "precision": "2", "width": "100"},
 		{"label": "Grand Total", "fieldname": "grand_total","fieldtype": "Float","precision": "2","width": "100"},

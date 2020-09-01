897f0b28e9a3741ced8c6e7e407ebb24c554fc0b
jiloysss <jangeles@bai.ph>
2020-09-01 19:05:49 +0800

    Update

--- service_pro/public/js/sales_invoice.js
+++ service_pro/public/js/sales_invoice.js
@@ -5,7 +5,9 @@ cur_frm.cscript.customer = function(frm){
 }
 cur_frm.cscript.onload = function(frm){
     filter_link_field(cur_frm)
-
+    if(frappe.boot.user.roles.includes("Accounts Manager") || frappe.boot.user.roles.includes("System Manager")){
+            cur_frm.set_df_property("paid", "allow_on_submit", 1)
+    }
 }
 
 function filter_link_field(cur_frm) {

ee6025e6e524b23dbbe77749a540d3ff3f1b9c80
jiloysss <jangeles@bai.ph>
2020-09-17 21:51:46 +0800

    Update

--- service_pro/service_pro/doctype/payment_scheduler/payment_scheduler.js
+++ service_pro/service_pro/doctype/payment_scheduler/payment_scheduler.js
@@ -1,56 +1,52 @@
 // Copyright (c) 2020, jan and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Payment Scheduler', {
-	// refresh: function(frm) {
+frappe.ui.form.on('Payment Scheduler','refresh', function(frm) {
+    if(cur_frm.doc.type === "Expense Claim"){
+        frm.fields_dict['details'].grid.get_field('doc1').get_query = function(doc, cdt, cdn) {
+            var child = locals[cdt][cdn];
+			return{
+                filters:[
+                    ['employee', '=', child.name1],
+                    ['status', '=', "Unpaid"],
+                ]
+            }
+        }
+    } else if (cur_frm.doc.type === "Supplier" || cur_frm.doc.type === "Agent") {
+        frm.fields_dict['details'].grid.get_field("doc1").get_query = function (doc, cdt, cdn) {
+            return {
+                filters: [
+                    ["status", "in", ["Paid", "Unpaid"]]
+                ]
+            }
+        }
+    }
 
-	// }
+
 });
 
 
 cur_frm.cscript.type = function (frm, cdt, cdn) {
-    filter_all(cur_frm, cdt, cdn, '')
-
-}
-
-function filter_all(cur_frm, cdt, cdn, employee) {
     var row = locals[cdt][cdn]
-    console.log(employee)
 
-    if(!employee){
-        cur_frm.clear_table("details")
-        cur_frm.refresh_field("details")
-    }
+    cur_frm.clear_table("details")
+    cur_frm.refresh_field("details")
     var name1 = frappe.meta.get_docfield("Payment Scheduler Details", "name1", cur_frm.doc.name);
     var doc1 = frappe.meta.get_docfield("Payment Scheduler Details", "doc1", cur_frm.doc.name);
-    name1.options = cur_frm.doc.type === "Supplier" ? cur_frm.doc.type : cur_frm.doc.type === "Expense Claim" ? "Employee" : ""
+    name1.options = cur_frm.doc.type === "Supplier" ? cur_frm.doc.type : cur_frm.doc.type === "Expense Claim" ? "Employee" : "Sales Person"
 
-    if(cur_frm.doc.type === "Supplier" && cur_frm.doc.type === "Agent"){
+    if(cur_frm.doc.type === "Supplier" || cur_frm.doc.type === "Agent"){
         doc1.options = "Sales Invoice"
-        cur_frm.fields_dict.details.grid.get_field("doc1").get_query =
-                function() {
-                    return {
-                          filters: [
-                                ["status", "in", ["Paid", "Unpaid"]]
-                            ]
-                    }
-                }
+                cur_frm.trigger("refresh")
 
 
     } else if (cur_frm.doc.type === "Expense Claim"){
         doc1.options = "Expense Claim"
-
-        cur_frm.set_query("doc1", "details", () => {
-        return {
-            filters: {
-                status: "Unpaid"
-        }
-        }
-    });
-            cur_frm.refresh_field("details")
-
+        cur_frm.trigger("refresh")
     }
+
 }
+
 cur_frm.cscript.doc1 = function (frm, cdt, cdn) {
     var row = locals[cdt][cdn]
     if(row.doc1 && cur_frm.doc.type === "Supplier"){
@@ -82,8 +78,8 @@ cur_frm.cscript.name1 = function (frm, cdt, cdn) {
             row.employee_name = employee_name.message.employee_name
             cur_frm.refresh_field("details")
         })
-        filter_all(cur_frm, cdt, cdn,row.name1)
 
 
     }
+
 }

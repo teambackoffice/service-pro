fa2cb4038f668fd48318acb9b77cb9313c1fc1fa
jiloysss <jangeles@bai.ph>
2020-09-22 22:57:10 +0800

    Update

--- service_pro/doc_events/journal_entry.py
+++ service_pro/doc_events/journal_entry.py
@@ -4,3 +4,7 @@ def submit_jv(doc, method):
     if doc.agent_payment_request:
         frappe.db.sql(""" UPDATE `tabAgent Payment Request` SET agent_outstanding_amount=0, status='Paid' WHERE name=%s """, doc.agent_payment_request)
         frappe.db.commit()
+
+    if doc.petty_cash_request:
+        frappe.db.sql(""" UPDATE `tabPetty Cash Request` SET agent_outstanding_amount=0, status='Paid' WHERE name=%s """, doc.petty_cash_request)
+        frappe.db.commit()

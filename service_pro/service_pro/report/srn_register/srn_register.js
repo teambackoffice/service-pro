1aa3f64588fc4ba593add9e2f87a1a676dfd3422
sammish <sammish.thundiyil@gmail.com>
2020-12-17 17:33:25 +0300

    Report Update

---
+++ service_pro/service_pro/report/srn_register/srn_register.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2016, jan and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["SRN Register"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label":__("From Date"),
+			"fieldtype": "Date",
+		},
+		{
+			"fieldname":"to_date",
+			"label": __ ("To Date"),
+			"fieldtype": "Date",
+		},
+		{
+			"fieldname":"customer",
+			"label": __ ("Customer Name"),
+			"fieldtype": "Link",
+			"options": "Customer",
+		},
+		{
+			"fieldname":"sales_man",
+			"label": __ ("Sales Man Name"),
+			"fieldtype": "Link",
+			"options": "Employee",
+		},
+		{
+			"fieldname": "status",
+			"label":__("Status"),
+			"fieldtype": "Select",
+			"options": "\nClosed\nTo Inspection\nTo Production",
+		}
+
+	]
+};

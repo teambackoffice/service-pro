59b2deea39b09bc8fc3866cf10df9533eb032877
sammish <sammish.thundiyil@gmail.com>
2020-12-29 08:49:45 +0000

    Report Update

--- service_pro/service_pro/report/delivery_register/delivery_register.js
+++ service_pro/service_pro/report/delivery_register/delivery_register.js
@@ -30,7 +30,7 @@ frappe.query_reports["Delivery Register"] = {
 			"fieldname": "status",
 			"label":__("Status"),
 			"fieldtype": "Select",
-			"options": "\nTo Bill\nCompleted\nClosed\nDraft",
+			"options": "\nTo Bill\nCompleted\nClosed\nReturn Issued\nReturn\nDraft",
 		}
 	]
 };

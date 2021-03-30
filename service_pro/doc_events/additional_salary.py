9d80da38cbcbb248b809be4d72a183dfa57b12e2
jiloysss <jangeles@bai.ph>
2021-03-30 17:26:23 +0800

    Update

--- service_pro/doc_events/additional_salary.py
+++ service_pro/doc_events/additional_salary.py
@@ -3,12 +3,10 @@ import frappe
 
 
 @frappe.whitelist()
-def get_salary_structure(employee):
+def get_salary_structure(employee, total_working_hours):
     ss = frappe.db.sql(""" SELECT * FROM `tabSalary Structure Assignment` WHERE employee=%s ORDER BY from_date DESC LIMIT 1""", (employee), as_dict=1)
 
     total = 0
-    base = 0
     if len(ss) > 0:
-        total = (ss[0].base / 30 / 8) * 1.5
-        base = ss[0].base
-    return total, base
+        total = float((ss[0].base / 30 / 8) * 1.5) * float(total_working_hours)
+    return total
--- service_pro/public/js/additional_salary.js
+++ service_pro/public/js/additional_salary.js
@@ -9,32 +9,38 @@ cur_frm.cscript.salary_component = function () {
                 cur_frm.set_df_property("amount", "reqd", !cur_frm.doc.is_hour_based)
                 cur_frm.set_df_property("amount", "read_only", cur_frm.doc.is_hour_based)
                 cur_frm.set_df_property("total_working_hour", "hidden", !cur_frm.doc.is_hour_based)
-                if(sc.is_hour_based && cur_frm.doc.employee){
-                    compute_total_working_hrs(cur_frm)
-                } else {
-                    cur_frm.doc.amount = 0
-                    cur_frm.doc.total_working_hour = 0
-                    cur_frm.refresh_field("amount")
-                    cur_frm.refresh_field("total_working_hour")
-
-                }
+                 compute_total_working_hrs(cur_frm)
             })
     }
 }
-
+cur_frm.cscript.total_working_hour = function () {
+    compute_total_working_hrs(cur_frm)
+}
 function compute_total_working_hrs(cur_frm) {
 
-    frappe.call({
-        method: "service_pro.doc_events.additional_salary.get_salary_structure",
-        args: {
-            employee: cur_frm.doc.employee
-        },
-        callback: function (r) {
-                cur_frm.doc.amount = r.message[1]
-                cur_frm.doc.total_working_hour = r.message[0]
-                cur_frm.refresh_field("amount")
-                cur_frm.refresh_field("total_working_hour")
+    if(cur_frm.doc.is_hour_based && cur_frm.doc.employee && cur_frm.doc.total_working_hour > 0){
+        console.log("NAA MAAAAN")
+        frappe.call({
+            method: "service_pro.doc_events.additional_salary.get_salary_structure",
+            args: {
+                employee: cur_frm.doc.employee,
+                total_working_hours: cur_frm.doc.total_working_hour
+            },
+            callback: function (r) {
+                console.log("MESSAGE")
+                console.log(r.message)
+                    cur_frm.doc.amount = r.message
+                    cur_frm.refresh_field("amount")
 
-        }
-    })
+            }
+        })
+    } else {
+        cur_frm.doc.amount = 0
+        cur_frm.doc.total_working_hour = 0
+        cur_frm.refresh_field("amount")
+        cur_frm.refresh_field("total_working_hour")
+
+    }
+
+
 }

542f836ff3064507d035fea94f484f1bb6347e4a
jiloysss <janlloydangeles@gmail.com>
2022-08-10 13:07:19 +0800

    Update

--- service_pro/service_pro/report/stock_list_report/stock_list_report.py
+++ service_pro/service_pro/report/stock_list_report/stock_list_report.py
@@ -35,22 +35,35 @@ def execute(filters=None):
 
 	for item in items:
 		item_defaults = frappe.db.sql(""" SELECT * FROM `tabItem Default` WHERE parent=%s and idx=1 """, item.name, as_dict=1)
-		item_price = frappe.db.sql(""" SELECT * FROM `tabItem Price` WHERE item_code=%s and selling=1 ORDER BY creation DESC LIMIT 1 """, item.item_code, as_dict=1)
+		item_price = frappe.db.sql(""" SELECT * FROM `tabItem Price` WHERE item_code=%s and selling=1 ORDER BY creation DESC """, item.item_code, as_dict=1)
 		lcr = frappe.db.sql(""" SELECT LCI.applicable_charges, LCI.qty FROM `tabLanded Cost Voucher` LCV 
 								INNER JOIN `tabLanded Cost Item` LCI ON LCI.parent = LCV.name 
 								WHERE LCI.item_code=%s and LCV.docstatus = 1 ORDER BY LCV.creation DESC LIMIT 1 """, item.name, as_dict=1)
+		if len(item_price) == 0:
+			add_item_without_price_list(item, item_price,lcr,item_defaults,filters,data)
+		else:
+			add_item_with_price_list(item, item_price, lcr, item_defaults, filters, data)
+	print(columns)
+	print(data)
+	return columns, data
+def add_item_with_price_list(item, item_price,lcr,item_defaults,filters,data):
+	for i in item_price:
 		obj = {
 			"item_code": item.item_code,
 			"item_name": item.item_name,
 			"valuation_rate": item.valuation_rate,
-			"selling_rate": item_price[0].price_list_rate if len(item_price) > 0 else 0,
-			"last_purchase_rate": get_last_purchase_rate(item.item_code) ,
-			"landed_cost_rate": get_last_purchase_rate(item.item_code) + (lcr[0].applicable_charges / lcr[0].qty) if len(lcr) > 0 and lcr[0].applicable_charges else get_last_purchase_rate(item.item_code) ,
-			"available_stock": get_previous_stock(item.item_code, item_defaults[0].default_warehouse if len(item_defaults) > 0 else "", date),
-			"price_list": item_price[0].price_list if len(item_price) > 0 else "",
+			"selling_rate": i.price_list_rate,
+			"last_purchase_rate": get_last_purchase_rate(item.item_code),
+			"landed_cost_rate": get_last_purchase_rate(item.item_code) + (lcr[0].applicable_charges / lcr[0].qty) if len(
+				lcr) > 0 and lcr[0].applicable_charges else get_last_purchase_rate(item.item_code),
+			"available_stock": get_previous_stock(item.item_code,
+												  item_defaults[0].default_warehouse if len(item_defaults) > 0 else "",
+												  date),
+			"price_list": i.price_list,
 		}
 		if filters.get("ignore_zero_stock") and not filters.get("ignore_negative_stock"):
-			if get_previous_stock(item.item_code, item_defaults[0].default_warehouse if len(item_defaults) > 0 else "", date) != 0:
+			if get_previous_stock(item.item_code, item_defaults[0].default_warehouse if len(item_defaults) > 0 else "",
+								  date) != 0:
 				data.append(obj)
 
 		elif filters.get("ignore_negative_stock") and not filters.get("ignore_zero_stock"):
@@ -64,10 +77,36 @@ def execute(filters=None):
 				data.append(obj)
 		else:
 			data.append(obj)
-	print(columns)
-	print(data)
-	return columns, data
+def add_item_without_price_list(item, item_price,lcr,item_defaults,filters,data):
+	obj = {
+		"item_code": item.item_code,
+		"item_name": item.item_name,
+		"valuation_rate": item.valuation_rate,
+		"selling_rate": item_price[0].price_list_rate if len(item_price) > 0 else 0,
+		"last_purchase_rate": get_last_purchase_rate(item.item_code),
+		"landed_cost_rate": get_last_purchase_rate(item.item_code) + (lcr[0].applicable_charges / lcr[0].qty) if len(
+			lcr) > 0 and lcr[0].applicable_charges else get_last_purchase_rate(item.item_code),
+		"available_stock": get_previous_stock(item.item_code,
+											  item_defaults[0].default_warehouse if len(item_defaults) > 0 else "",
+											  date),
+		"price_list": item_price[0].price_list if len(item_price) > 0 else "",
+	}
+	if filters.get("ignore_zero_stock") and not filters.get("ignore_negative_stock"):
+		if get_previous_stock(item.item_code, item_defaults[0].default_warehouse if len(item_defaults) > 0 else "",
+							  date) != 0:
+			data.append(obj)
 
+	elif filters.get("ignore_negative_stock") and not filters.get("ignore_zero_stock"):
+		if get_previous_stock(item.item_code, item_defaults[0].default_warehouse if len(item_defaults) > 0 else "",
+							  date) >= 0:
+			data.append(obj)
+
+	elif filters.get("ignore_negative_stock") and filters.get("ignore_zero_stock"):
+		if get_previous_stock(item.item_code, item_defaults[0].default_warehouse if len(item_defaults) > 0 else "",
+							  date) > 0:
+			data.append(obj)
+	else:
+		data.append(obj)
 def get_previous_stock(item_code, warehouse, date):
 	previous_sle = get_previous_sle({
 		"item_code": item_code,

0d27fc41d77a74c2a141ce782d7e0d6e6f529b25
sammish <sammish.thundiyil@gmail.com>
2023-09-02 08:52:44 +0000

    update doctype

---
+++ service_pro/service_pro/doctype/expense_component/expense_component.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, jan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Expense Component', {
+	// refresh: function(frm) {
+
+	// }
+});

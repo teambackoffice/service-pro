c3a3257e0abb0036ae1142d1b6475fc2e9fe93bd
jiloysss <janlloydangeles@gmail.com>
2024-03-31 11:02:09 +0800

    Update

--- service_pro/doc_events/landed_cost_voucher.py
+++ service_pro/doc_events/landed_cost_voucher.py
@@ -6,9 +6,11 @@ def get_item_avail_qty(items):
     avail_qty = []
     items = json.loads(items)
     for item in items:
+        warehouse = frappe.db.sql(""" SELECT * FROM `tab{0}` WHERE name=%s""".format(item.get("receipt_document_type")),item.get("receipt_document"),as_dict=1)
+
         if item.get("item_code"):
             if frappe.db.exists("Bin", {"item_code":item.get("item_code")}):
-                bin = frappe.get_last_doc("Bin", {"item_code":item.get("item_code")})
+                bin = frappe.get_last_doc("Bin", {"item_code":item.get("item_code"),"warehouse":warehouse[0].set_warehouse})
                 avail_qty.append({"item_code": item.get("item_code"), "avail_qty":bin.actual_qty})
     return avail_qty
 

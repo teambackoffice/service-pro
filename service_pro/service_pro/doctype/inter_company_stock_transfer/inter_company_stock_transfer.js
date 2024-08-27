1ef9e3f7fee3adb375c5d64246fa400bf76e69cd
jiloysss <janlloydangeles@gmail.com>
2024-08-27 11:36:06 +0800

    Update

--- service_pro/service_pro/doctype/inter_company_stock_transfer/inter_company_stock_transfer.js
+++ service_pro/service_pro/doctype/inter_company_stock_transfer/inter_company_stock_transfer.js
@@ -1,8 +1,113 @@
 // Copyright (c) 2024, jan and contributors
 // For license information, please see license.txt
+var defaults = {}
+function compute_totals(cur_frm) {
+    var total_qty = 0
+    var debit_value = 0
+    var credit_value = 0
+     for(var x=0;x<cur_frm.doc.item_details.length;x+=1){
+         if(cur_frm.doc.item_details[x].qty > cur_frm.doc.item_details[x].available_qty){
+             cur_frm.doc.item_details[x].qty = 0
+             cur_frm.refresh_field("item_details")
+             frappe.throw("Qty Cannot Be More Than Available Qty")
+         }
+          total_qty += cur_frm.doc.item_details[x].qty
+          debit_value += cur_frm.doc.item_details[x].value
+          credit_value += cur_frm.doc.item_details[x].credit_value
 
-// frappe.ui.form.on("Inter Company Stock Transfer", {
-// 	refresh(frm) {
+      }
+    cur_frm.doc.deference_value = debit_value - credit_value
+    cur_frm.doc.total_qty = total_qty
+    cur_frm.doc.debit_value = debit_value
+    cur_frm.doc.credit_value = credit_value
+    cur_frm.refresh_fields(["deference_value","total_qty","debit_value","credit_value"])
+}
+frappe.ui.form.on("Inter Company Stock Transfer Item", {
+    item_code: function (frm,cdt,cdn) {
+        var d = locals[cdt][cdn]
+        if(d.item_code){
+            cur_frm.call({
+                doc: cur_frm.doc,
+                method: "get_avail_qty",
+                args: {
+                  item: d
+                },
+                freeze: true,
+                freeze_message: "Getting Available Qty",
+                callback: function (r) {
+                    d.available_qty = r.message.actual_qty
+                    d.value = r.message.valuation_rate
+                    cur_frm.refresh_field(d.parentfield)
+                    compute_totals(cur_frm)
 
-// 	},
-// });
+                }
+            })
+
+        }
+        
+    },
+     qty: function (frm,cdt,cdn) {
+       compute_totals(cur_frm)
+
+    },
+     credit_value: function (frm,cdt,cdn) {
+       compute_totals(cur_frm)
+
+    },
+    item_details_remove: function () {
+        compute_totals(cur_frm)
+    },
+})
+frappe.ui.form.on("Inter Company Stock Transfer", {
+
+    auto_fill_credit_value: function () {
+      for(var x=0;x<cur_frm.doc.item_details.length;x+=1){
+          cur_frm.doc.item_details[x].credit_value =cur_frm.doc.item_details[x].value
+          cur_frm.refresh_field("item_details")
+      }
+       compute_totals(cur_frm)
+    },
+	from_company: function () {
+       if(cur_frm.doc.company){
+            cur_frm.call({
+                doc: cur_frm.doc,
+                method: "get_defaults",
+                freeze: true,
+                freeze_message: "Getting Company Defaults....",
+                callback: function (r) {
+                    defaults = r.message
+
+                }
+            })
+        }
+    },
+    to_company: function () {
+       if(cur_frm.doc.company){
+            cur_frm.call({
+                doc: cur_frm.doc,
+                method: "get_defaults",
+                freeze: true,
+                freeze_message: "Getting Company Defaults....",
+                callback: function (r) {
+                    defaults = r.message
+
+                }
+            })
+        }
+    },
+    onload: function (frm) {
+	    if(cur_frm.is_new()){
+	       cur_frm.call({
+                doc: cur_frm.doc,
+                method: "get_defaults",
+                freeze: true,
+                freeze_message: "Getting Company Defaults....",
+                callback: function (r) {
+                    defaults = r.message
+
+                }
+            })
+        }
+
+    }
+});
--- service_pro/service_pro/doctype/inter_company_stock_transfer/inter_company_stock_transfer.py
+++ service_pro/service_pro/doctype/inter_company_stock_transfer/inter_company_stock_transfer.py
@@ -1,9 +1,98 @@
 # Copyright (c) 2024, jan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class InterCompanyStockTransfer(Document):
-	pass
+	def on_submit(self):
+		self.first_se()
+		self.second_se()
+	def first_se(self):
+		obj = {
+			"doctype": "Stock Entry",
+			"stock_entry_type": "Material Issue",
+			"posting_date":self.posting_date,
+			"posting_time":self.time,
+			"from_warehouse": self.from_warehouse,
+			"company": self.from_company,
+			"custom_inter_company_stock_transfer": self.name
+
+		}
+		items= []
+		for x in self.item_details:
+			items.append({
+				"item_code": x.item_code,
+				"qty": x.qty,
+				"s_warehouse": self.from_warehouse,
+				"basic_rate": x.value,
+				"expense_account": self.from_company_debit_account,
+				"cost_center": self.from_cost_center,
+			})
+		obj['items'] = items
+		se = frappe.get_doc(obj).insert(ignore_permissions=1)
+		se.submit()
+	def second_se(self):
+		obj = {
+			"doctype": "Stock Entry",
+			"stock_entry_type": "Material Receipt",
+			"posting_date":self.posting_date,
+			"posting_time":self.time,
+			"to_warehouse": self.from_warehouse,
+			"company": self.to_company,
+			"custom_inter_company_stock_transfer": self.name
+		}
+		items = []
+		for x in self.item_details:
+			items.append({
+				"item_code": x.item_code,
+				"qty": x.qty,
+				"s_warehouse": self.from_warehouse,
+				"basic_rate": x.credit_value,
+				"expense_account": self.to_company_credit_account,
+				"cost_center": self.to_cost_center,
+			})
+		obj['items'] = items
+		se = frappe.get_doc(obj).insert(ignore_permissions=1)
+		se.submit()
+	@frappe.whitelist()
+	def get_avail_qty(self,item):
+		bin = frappe.db.sql(""" SELECT * FROM `tabBin` WHERE item_code=%s and warehouse=%s """,(item['item_code'],self.from_warehouse),as_dict=1)
+
+		return bin[0] if len(bin) > 0 else 0
+
+	@frappe.whitelist()
+	def get_defaults(self):
+		print("--------------------------------------------------------------------------")
+		defaults = {}
+
+		if self.from_company:
+			tables = [
+				"Inter Company Stock Transfer From",
+			]
+			for table in tables:
+				data = frappe.db.sql(""" SELECT * FROM `tab{0}` WHERE company=%s """.format(table), self.from_company,
+									 as_dict=1)
+				if len(data) > 0:
+					defaults[data[0].parentfield] = data[0]
+
+			self.from_warehouse = defaults['from_companies'].warehouse if 'from_companies' in defaults else ""
+			self.from_company_debit_account = defaults['from_companies'].account if 'from_companies' in defaults else ""
+			self.from_cost_center = defaults['from_companies'].cost_center if 'from_companies' in defaults else ""
+
+		if self.to_company:
+			tables = [
+				"Inter Company Stock Transfer To",
+			]
+			for table in tables:
+				data = frappe.db.sql(""" SELECT * FROM `tab{0}` WHERE company=%s """.format(table), self.to_company,
+									 as_dict=1)
+				if len(data) > 0:
+					defaults[data[0].parentfield] = data[0]
+
+			self.to_warehouse = defaults['to_companies'].warehouse if 'to_companies' in defaults else ""
+			self.to_company_credit_account = defaults['to_companies'].account if 'to_companies' in defaults else ""
+			self.to_cost_center = defaults['to_companies'].cost_center if 'from_companies' in defaults else ""
+
+		return defaults

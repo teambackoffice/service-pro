c30031a0e897883c262a81bc0f78390645bc1f58
Ashkar <muhammed786ashkar@gmail.com>
2024-09-17 11:58:38 +0530

    feat: inter company stock transfer modified

--- service_pro/service_pro/doctype/production_settings/production_settings.py
+++ service_pro/service_pro/doctype/production_settings/production_settings.py
@@ -7,13 +7,4 @@ import frappe
 from frappe.model.document import Document
 
 class ProductionSettings(Document):
-	@frappe.whitelist()
-	def get_series(self):
-		ps = frappe.db.sql(""" SELECT * FROM `tabProperty Setter` WHERE name='Item-naming_series-options'""", as_dict=1)
-		naming_seriess = frappe.db.sql(
-			""" SELECT fieldname, options FROM `tabDocField` WHERE parent='Item' AND fieldname='naming_series'; """)
-
-		if len(ps) > 0:
-			return ps[0].value
-		if len(naming_seriess) > 0:
-			return naming_seriess[0].options
+    pass

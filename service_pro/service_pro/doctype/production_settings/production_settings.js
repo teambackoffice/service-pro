3ac67cfc2c93ea20704228b2e25d57b7bdbb12ef
Ashkar <muhammed786ashkar@gmail.com>
2024-10-04 10:25:36 +0530

    fix: Serivice order form customize

--- service_pro/service_pro/doctype/production_settings/production_settings.js
+++ service_pro/service_pro/doctype/production_settings/production_settings.js
@@ -1,3 +1,77 @@
 // // Copyright (c) 2020, jan and contributors
 // // For license information, please see license.txt
 
+frappe.ui.form.on('Production Settings', {
+    refresh: function(frm) {
+        frm.fields_dict['finish_good_defaults'].grid.get_field('income_account').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            if (row.company) {
+                return {
+                    filters: {
+                        company: row.company
+                    }
+                };
+            }
+        };
+        frm.fields_dict['finish_good_defaults'].grid.get_field('finish_good_warehouse').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            if (row.company) {
+                return {
+                    filters: {
+                        company: row.company
+                    }
+                };
+            }
+        };
+        frm.fields_dict['finish_good_defaults'].grid.get_field('finish_good_cost_center').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            if (row.company) {
+                return {
+                    filters: {
+                        company: row.company
+                    }
+                };
+            }
+        };
+        frm.fields_dict['raw_material_defaults'].grid.get_field('warehouse').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            if (row.company) {
+                return {
+                    filters: {
+                        company: row.company
+                    }
+                };
+            }
+        };
+        frm.fields_dict['raw_material_defaults'].grid.get_field('cost_center').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            if (row.company) {
+                return {
+                    filters: {
+                        company: row.company
+                    }
+                };
+            }
+        };
+        frm.fields_dict['site_job_report_settings'].grid.get_field('warehouse').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            if (row.company) {
+                return {
+                    filters: {
+                        company: row.company
+                    }
+                };
+            }
+        };
+        frm.fields_dict['site_job_report_settings'].grid.get_field('cost_center').get_query = function(doc, cdt, cdn) {
+            let row = locals[cdt][cdn];
+            if (row.company) {
+                return {
+                    filters: {
+                        company: row.company
+                    }
+                };
+            }
+        };
+    }
+});

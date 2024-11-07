7f493adf408cb915314f44bd2566fe62ab68cbde
Jishnu-Epics <jishnu2016@gmail.com>
2024-11-07 17:30:04 +0530

    fix:custom field issue

--- service_pro/doc_events/after_migrate.py
+++ service_pro/doc_events/after_migrate.py
@@ -3,7 +3,7 @@ import frappe
 
 @frappe.whitelist()
 def execute():
-    fields = ["Customer-sales_man","Customer-sales_man_name"]
+    fields = ["Customer-sales_man","Customer-sales_man_name","Sales Invoice-sales_man_name","Sales Invoice-sales_man"]
 
     for x in fields:
         frappe.db.sql(""" DELETE FROM `tabCustom Field` WHERE name=%s """,x)

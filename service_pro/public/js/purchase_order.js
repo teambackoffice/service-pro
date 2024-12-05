e125ae72bb1c921bc47e57f97d3ce67bebfa1bb3
Ashkar <muhammed786ashkar@gmail.com>
2024-12-05 15:54:47 +0530

    feat: purchase order

--- service_pro/public/js/purchase_order.js
+++ service_pro/public/js/purchase_order.js
@@ -21,3 +21,111 @@ frappe.ui.form.on('Purchase Order', {
     }
 },
 })
+
+
+frappe.ui.form.on('Purchase Order Item', {
+    request: function (frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        reset_fields(cdt, cdn);
+
+        if (row.request === 'Stock' && row.item_code) {
+            fetch_available_qty(row, frm.doc.transaction_date);
+        } else if (row.request === 'Sales Order' && row.sales_order) {
+            fetch_sales_order_id(row);
+        } else if (row.request === 'Material Request' && row.material_request) {
+            fetch_material_request_id(row);
+        } else if (row.request === 'Sales Order') {
+            frappe.msgprint(__('There is no purchase order against sales order.'));
+        }
+    },
+    sales_order: function (frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (row.sales_order) {
+            fetch_sales_order_id(row);
+        }
+    },
+    material_request: function (frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (row.material_request) {
+            fetch_material_request_id(row);
+        }
+    },
+    item_code: function (frm, cdt, cdn) {
+        reset_fields(cdt, cdn);
+    }
+});
+
+function reset_fields(cdt, cdn) {
+    frappe.model.set_value(cdt, cdn, 'aval_qty', null);
+    frappe.model.set_value(cdt, cdn, 'order_id', null);
+    frappe.model.set_value(cdt, cdn, 'request_id', null);
+}
+
+function fetch_available_qty(row, transaction_date) {
+    frappe.call({
+        method: "service_pro.doc_events.purchase_order.get_available_qty",
+        args: {
+            item_code: row.item_code,
+            warehouse: row.warehouse,
+            posting_date: transaction_date || frappe.datetime.now_date()
+        },
+        callback: function (r) {
+            if (r.message !== undefined) {
+                const availableQty = r.message;
+
+                frappe.model.set_value(row.doctype, row.name, 'aval_qty', availableQty);
+
+                if (availableQty === 0) {
+                    frappe.msgprint({
+                        title: __('No Quantity Available'),
+                        message: __('Not Qty Available This Item'),
+                        indicator: 'red'
+                    });
+                }
+            } 
+        }
+    });
+}
+
+function fetch_sales_order_id(row) {
+    frappe.call({
+        method: "service_pro.doc_events.purchase_order.get_sales_order_id",
+        args: {
+            sales_order: row.sales_order,
+            item_code: row.item_code
+        },
+        callback: function (r) {
+            if (r.message) {
+                frappe.model.set_value(row.doctype, row.name, 'order_id', r.message);
+            } else {
+                frappe.msgprint({
+                    title: __('Error'),
+                    message: __('There is no purchase order against sales order.'),
+                    indicator: 'red'
+                });
+            }
+        }
+    });
+}
+
+function fetch_material_request_id(row) {
+    frappe.call({
+        method: "service_pro.doc_events.purchase_order.get_material_request_id",
+        args: {
+            material_request: row.material_request,
+            item_code: row.item_code
+        },
+        callback: function (r) {
+            if (r.message) {
+                frappe.model.set_value(row.doctype, row.name, 'request_id', r.message);
+            } else {
+                frappe.msgprint({
+                    title: __('Error'),
+                    message: __('There is no purchase order against material request.'),
+                    indicator: 'red'
+                });
+            }
+        }
+    });
+}
+

4950d5bbc9326d768265f13c1c42e5178a5c6c2f
salmanadayatt <salusalman731@gmail.com>
2025-02-06 14:46:25 +0530

    additional salary

--- service_pro/doc_events/additional_salary.py
+++ service_pro/doc_events/additional_salary.py
@@ -6,7 +6,14 @@ import frappe
 def get_salary_structure(employee, total_working_hours):
     ss = frappe.db.sql(""" SELECT * FROM `tabSalary Structure Assignment` WHERE employee=%s ORDER BY from_date DESC LIMIT 1""", (employee), as_dict=1)
 
+    base = ss.base
+
+    basic_salary = base/100*75
+    house_rent_allowance = base/100*18.75
+
+    gp = basic_salary + house_rent_allowance
+
     total = 0
     if len(ss) > 0:
-        total = float((ss[0].base / 30 / 8) * 1.5) * float(total_working_hours)
+        total = float((ss[0].gp / 30 / 8) * 1.5) * float(total_working_hours)
     return total

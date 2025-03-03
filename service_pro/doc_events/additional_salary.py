8f84dc122a9025d9685aa31958386cc8e88c683e
Jishnu-Epics <jishnuvv2016@gmail.com>
2025-03-03 14:37:27 +0530

    fix: overtime issue

--- service_pro/doc_events/additional_salary.py
+++ service_pro/doc_events/additional_salary.py
@@ -3,10 +3,8 @@ import frappe
 @frappe.whitelist()
 def get_salary_structure(employee, total_working_hours):
     ss = frappe.db.sql(
-        """ SELECT * FROM `tabSalary Structure Assignment` WHERE employee=%s ORDER BY from_date DESC LIMIT 1""", 
-        (employee,), 
-        as_dict=True
-    )
+    """ 
+    SELECT * FROM `tabSalary Structure Assignment` WHERE employee=%s AND docstatus=1 ORDER BY from_date DESC LIMIT 1""", (employee,), as_dict=True)
 
     salary_structure = ss[0]
 

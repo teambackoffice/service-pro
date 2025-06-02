bd4c007027c51ed0fb4b33e5cca0dd36e72861f8
Ashkar <muhammed786ashkar@gmail.com>
2025-06-02 21:38:00 +0530

    item doc in valuation rate hide

--- service_pro/service_pro/doctype/inter_company_stock_transfer/inter_company_stock_transfer_dashboard.py
+++ service_pro/service_pro/doctype/inter_company_stock_transfer/inter_company_stock_transfer_dashboard.py
@@ -4,9 +4,13 @@ from frappe import _
 def get_data():
 	return {
 		'fieldname': 'custom_inter_company_stock_transfer',
+		'internal_links': {
+			'Inter Company Material Request': 'inter_company_material_request'
+		},
 		'transactions': [
 			{
-				'label': _('Reference'),'items': [ "Stock Entry"]
+				'label': _('Reference'),
+				'items': ['Stock Entry', "Inter Company Material Request"],
 			}
-		]
+		],
 	}

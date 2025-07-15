369bf9af17166db1ab4fb22562a778bc214f23d7
vijilavk <vijilavk46@gmail.com>
2025-07-15 15:27:52 +0530

    feat:filters added in intercompany report

--- service_pro/service_pro/report/inter_company_stock_transfer_report/inter_company_stock_transfer_report.py
+++ service_pro/service_pro/report/inter_company_stock_transfer_report/inter_company_stock_transfer_report.py
@@ -1,10 +1,5 @@
-# ERPNext Script Report for "Inter Company Stock Transfer"
-# Doctype: Inter Company Stock Transfer
-# Child Table: item_details (fields: item_code, qty, value)
-
 import frappe
 
-# py code
 def execute(filters=None):
     columns = [
         {"label": "ID", "fieldname": "name", "fieldtype": "Data", "width": 150},
@@ -27,23 +22,25 @@ def execute(filters=None):
     data = []
     total_issued_qty = total_received_qty = total_debit = total_credit = 0
 
+    # Prepare filters
+    transfer_filters = {
+        key: val for key, val in (filters or {}).items()
+        if key in ["from_company", "to_company", "status", "name", "posting_date"]
+    }
+
+    # Range filter
+    if filters and filters.get("from_date") and filters.get("to_date"):
+        transfer_filters["posting_date"] = ["between", [filters["from_date"], filters["to_date"]]]
+
+    # Fetch transfer documents
     transfers = frappe.get_all(
         "Inter Company Stock Transfer",
-        filters={
-            **{k: v for k, v in (filters or {}).items() if k in ["from_company", "to_company", "status"]},
-            "posting_date": ["between", [filters.get("from_date"), filters.get("to_date")]] if filters else ["!=", ""]
-        },
+        filters=transfer_filters,
         fields=["name", "from_company", "to_company", "status", "posting_date"]
     )
 
     for transfer in transfers:
-        items = frappe.get_all(
-            "Inter Company Stock Transfer Item",
-            filters={"parent": transfer.name},
-            fields=["item_code", "qty", "value", "received_qty", "credit_value"]
-        )
-
-        # Add parent row (Transfer Header)
+        # Parent row
         data.append({
             "name": transfer.name,
             "from_company": transfer.from_company,
@@ -53,33 +50,46 @@ def execute(filters=None):
             "indent": 0
         })
 
+        # Child items
+        items = frappe.get_all(
+            "Inter Company Stock Transfer Item",
+            filters={"parent": transfer.name},
+            fields=["item_code", "qty", "value", "received_qty", "credit_value"]
+        )
+
         for item in items:
-            from_total = (item.qty or 0) * (item.value or 0)
-            to_total = (item.received_qty or 0) * (item.credit_value or 0)
-            qty_diff = (item.qty or 0) - (item.received_qty or 0)
+            qty = item.get("qty") or 0
+            value = item.get("value") or 0
+            received_qty = item.get("received_qty") or 0
+            credit_value = item.get("credit_value") or 0
+
+            from_total = qty * value
+            to_total = received_qty * credit_value
+            qty_diff = qty - received_qty
             value_diff = from_total - to_total
 
             data.append({
                 "name": f"{transfer.name}-{item.item_code}",
+                "parent_transfer": transfer.name,
                 "from_item": item.item_code,
-                "from_qty": item.qty,
-                "from_value": item.value,
+                "from_qty": qty,
+                "from_value": value,
                 "from_total": from_total,
                 "to_item": item.item_code,
-                "to_qty": item.received_qty,
-                "to_value": item.credit_value,
+                "to_qty": received_qty,
+                "to_value": credit_value,
                 "to_total": to_total,
                 "qty_diff": qty_diff,
                 "value_diff": value_diff,
                 "indent": 1
             })
 
-            total_issued_qty += item.qty or 0
-            total_received_qty += item.received_qty or 0
+            total_issued_qty += qty
+            total_received_qty += received_qty
             total_debit += from_total
             total_credit += to_total
 
-    # Append totals row
+    # Totals row
     data.append({
         "name": "TOTAL",
         "from_qty": total_issued_qty,

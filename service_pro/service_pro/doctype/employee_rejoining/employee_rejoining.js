cb8645285417d0a3a0616a005f19bf636b6e5df6
Ashkar <muhammed786ashkar@gmail.com>
2025-08-22 11:03:27 +0530

    rejoining changes

--- service_pro/service_pro/doctype/employee_rejoining/employee_rejoining.js
+++ service_pro/service_pro/doctype/employee_rejoining/employee_rejoining.js
@@ -1,77 +1,27 @@
 frappe.ui.form.on("Employee Rejoining", {
-    refresh: function(frm) {
-        calculate_all(frm);
-    },
-    gosi_basic_salary: function(frm) {
-        calculate_all(frm);
-    },
-    number_of_working_days: function(frm) {
-        calculate_all(frm);
-    },
-    travel_allowance: function(frm) {
-        calculate_all(frm);
-    },
-    bonus_allowance: function(frm) {
-        calculate_all(frm);
-    },
     employee_id: function(frm) {
-        calculate_all(frm);
+        if (frm.doc.employee_id && frm.doc.date_of_departure && frm.doc.last_rejoining_date) {
+            frm.trigger('recalculate');
+        }
     },
     date_of_departure: function(frm) {
-        calculate_all(frm);
+        frm.trigger('recalculate');
     },
     last_rejoining_date: function(frm) {
-        calculate_all(frm);
+        frm.trigger('recalculate');
     },
-    validate: function(frm) {
-        calculate_all(frm);
+    gosi_basic_salary: function(frm) {
+        frm.trigger('recalculate');
+    },
+    travel_allowance: function(frm) {
+        frm.trigger('recalculate');
+    },
+    bonus_allowance: function(frm) {
+        frm.trigger('recalculate');
+    },
+    
+    recalculate: function(frm) {
+        if (!frm.is_new() && frm.is_dirty()) {
+        }
     }
 });
-
-function calculate_all(frm) {
-    if (frm.doc.employee_id) {
-        let filters = {
-            employee: frm.doc.employee_id,
-            leave_type: "Leave Without Pay",
-            status: "Approved"
-        };
-        
-        if (frm.doc.date_of_departure && frm.doc.last_rejoining_date) {
-            filters["from_date"] = ["between", [frm.doc.date_of_departure, frm.doc.last_rejoining_date]];
-        }
-        
-        frappe.call({
-            method: "frappe.client.get_list",
-            args: {
-                doctype: "Leave Application",
-                filters: filters,
-                fields: ["total_leave_days"]
-            },
-            callback: function(r) {
-                let total_days = 0;
-                if (r.message) {
-                    r.message.forEach(row => {
-                        total_days += flt(row.total_leave_days);
-                    });
-                }
-                frm.set_value("leave_application_count", total_days);
-                
-                let working_days = 0;
-                if (frm.doc.count_vacation_days) {
-                    working_days = frm.doc.count_vacation_days - total_days;
-                    working_days = Math.max(0, working_days);
-                    frm.set_value("number_of_working_days", working_days);
-                }
-                
-                if (frm.doc.gosi_basic_salary && working_days) {
-                    let base_salary = (frm.doc.gosi_basic_salary / 30 / 12) * working_days;
-                    let travel = flt(frm.doc.travel_allowance);
-                    let bonus = flt(frm.doc.bonus_allowance);
-                    let total_salary = base_salary + travel + bonus;
-                    
-                    frm.set_value("employee_vacation_salary", total_salary);
-                }
-            }
-        });
-    }
-}
